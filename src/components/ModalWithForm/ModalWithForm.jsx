37adfc4f3aec815039f28f7554e7ccdcda8d0ecb
MikeFortKnox <134332447+MikeFortKnox@users.noreply.github.com>
2024-10-01 12:19:43 -0400

    corrections

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -2,7 +2,7 @@ import "./ModalWithForm.css";
 
 function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
-    <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
+    <div className isOpen={activeModal === "add-garment"}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
